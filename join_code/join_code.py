a3ee1c6417ad0fe413bc9f7f8e1310e923be617f
twinsliving <dominik.mg@gmx.de>
2025-10-16 19:37:31 +0200

    merges dataset

--- join_code/join_code.py
+++ join_code/join_code.py
@@ -12,4 +12,16 @@ import matplotlib.pyplot as plt
 # %%
 # import files
 df_GDP = pd.read_csv(r"clean_dataset\cleaned gdp_population_gdp per capita dataset.csv")
+df_pollutant = pd.read_csv(r"clean_dataset\pollutant_data.csv")
+# %%
+#check if it worked
+df_GDP
+df_pollutant
+# %%
+#merge datasets
+df_merged = pd.merge(df_GDP,df_pollutant,left_on=["Country Name", "Year"],right_on=["country", "year"],how="inner")
+print(df_merged.head())
+# %%
+# save merged dataset
+df_merged.to_csv("merged_dataset.csv", index=False)
 # %%
